84de3dbb5ff38c096bca953fdb3fb29392dc1e9d
QuanLew <quan,le01@sjsu.edu>
2023-12-07 01:36:54 -0800

    update

--- app/screens/Main.tsx
+++ app/screens/Main.tsx
@@ -9,7 +9,7 @@ import {
   StyleSheet,
   ActivityIndicator,
 } from "react-native";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Audio } from "expo-av";
 import axios from "axios";
 import * as FileSystem from "expo-file-system";
@@ -18,7 +18,15 @@ import { FontAwesome5 } from "@expo/vector-icons";
 import theme from "../../theme";
 
 const Main = ({ navigation }: any) => {
-  const colours = ['red', 'orange', 'yellow', 'blue', 'green', 'indigo', 'violet'];
+  const colours = [
+    "red",
+    "orange",
+    "yellow",
+    "blue",
+    "green",
+    "indigo",
+    "violet",
+  ];
   const getColour = () => colours[Math.floor(Math.random() * colours.length)];
   const [borderColor, setBorderColor] = useState(getColour());
   const [plainemail, setPlainEmail] = useState("");
@@ -26,24 +34,41 @@ const Main = ({ navigation }: any) => {
   const [recordings, setRecordings] = useState([]);
   const [transcription, setTranscription] = useState("");
   const [loading, setLoading] = useState(false);
-  
+
+  useEffect(() => {downloadAsset()}, []);
+
+  const downloadAsset = async () => {
+    const directory = `${FileSystem.cacheDirectory}ExponentAsset-b62641afc9ab487008e996a5c5865e56.ttf`;
+
+    // Check if the directory exists, create it if not
+    const directoryInfo = await FileSystem.getInfoAsync(directory);
+    if (!directoryInfo.exists) {
+      await FileSystem.makeDirectoryAsync(directory, { intermediates: true });
+    }
+
+    // Now, you can safely call downloadAsync
+    const downloadResult = await FileSystem.downloadAsync(
+      "https://example.com/path-to-your-asset.ttf",
+      directory
+    );
+
+    console.log("Download result:", downloadResult);
+  };
+
   async function startRecording() {
     try {
       console.log("Requesting permissions..");
       const { status } = await Audio.requestPermissionsAsync();
-
       if (status !== "granted") {
         console.error("Permission to access audio is denied");
         return;
       }
-
       // needed for IOS
       await Audio.setAudioModeAsync({
         allowsRecordingIOS: true,
         playsInSilentModeIOS: true,
         shouldDuckAndroid: true,
       });
-
       console.log("Starting recording..");
       const recording = new Audio.Recording();
       const { ios, android } = Audio.RecordingOptionsPresets.HIGH_QUALITY;
@@ -56,10 +81,9 @@ const Main = ({ navigation }: any) => {
         },
         web: {
           mimeType: "",
-          bitsPerSecond: 0
-        }
+          bitsPerSecond: 0,
+        },
       });
-
       await recording.startAsync();
       setRecording(recording);
       console.log("Recording started");
@@ -69,8 +93,16 @@ const Main = ({ navigation }: any) => {
   }
 
   async function stopRecording() {
+    // console.log("submit 1" + transcription);
+    // handleSubmit("http://127.0.0.1:4000/api/transcribe", "uri");
+    // console.log("submit 2: " + transcription);
+    //handleAskAI("http://localhost:4000/api/ask", transcription);
+    //console.log("submit 3");
+
     try {
       setRecording(undefined);
+      setTranscription("");
+      setPlainEmail("");
       await recording.stopAndUnloadAsync();
 
       let allRecordings = [...recordings];
@@ -85,13 +117,17 @@ const Main = ({ navigation }: any) => {
       const temp = await FileSystem.getInfoAsync(uri);
 
       if (!temp.exists) {
-        console.log("Gif directory doesn't exist, creating…");
+        console.log("Recording directory doesn't exist, creating…");
       } else {
         console.log("Created");
       }
+
       //console.log("Link type of URI: " + typeof uri);
       handleSubmit("http://localhost:4000/api/transcribe", uri);
-      handleAskAI("http://localhost:4000/api/ask", "How to change Fahrenheit to Celsius?");
+      handleAskAI(
+        "http://localhost:4000/api/ask",
+        "How to change Fahrenheit to Celsius?"
+      );
       console.log("Recording stopped and stored at", uri);
 
       setRecordings(allRecordings);
@@ -101,36 +137,133 @@ const Main = ({ navigation }: any) => {
   }
 
   useEffect(() => {
-    setTranscription(transcription)
-    console.log("useEffect: " + transcription);
+    console.log("submit effect: " + transcription);
+    setTranscription(transcription);
+    //handleAskAI("http://localhost:4000/api/ask", transcription);
   }, [transcription]);
 
+  // const getTranscription = useCallback(
+  //   (prevTranscription) => {
+  //     !prevTranscription === prevTranscription;
+  //   },
+  //   [transcription]
+  // );
+
+  // handle user's voice
   const handleSubmit = async (url: string, userData: string) => {
-    try {
-      const response = await axios.post(url, userData);
-      setTranscription(response.data.transcription)
-      console.log("res: " + transcription);
-    } catch (error) {
-      console.log(error);
-    }
+    // try {
+    //   setLoading(true);
+    //   await axios.post(url, userData).then((res) => {
+    //     console.log("submit reppp here: " + res.data.transcription);
+    //     setTranscription(res.data.transcription);
+    //     setLoading(false);
+    //   });
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    setLoading(true);
+    await axios
+      .post(url, userData)
+      .then(function (response) {
+        console.log("submit reppp here: " + response.data.transcription);
+        setTranscription(response.data.transcription);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        console.log("submit transcript the end: " + transcription);
+        handleAskAI("http://localhost:4000/api/ask", transcription);
+      });
   };
 
-  const handleAskAI = async (url: string, userData: string) => {
-    try {
-      setLoading(true)
-      const response = await axios
-      .post(url, userData)
-      .then((res) => {
-        console.log("reppp here: "+res.data.message);
-        setPlainEmail(res.data.message);
-        setLoading(false)
-      })
-      
-      //console.log("AI reply back: " + response.data.message);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // handle users' voice to plain email
+  const handleAskAI = useCallback(
+    async (url: string, userData: string) => {
+      // try {
+      //   //setLoading(true);
+      //   const checkmsg = userData.toLowerCase();
+      //   console.log("submit msg: " + checkmsg);
+      //   const keywords = ["email", "letter", "send", "write", "weather"]; // add more keywords
+      //   if (!keywords.some((keyword) => checkmsg.includes(keyword))) {
+      //     console.log("submit reppppp dont have kw");
+      //     setTranscription(
+      //       "I'm your email bot, ask me anything related to email"
+      //     );
+      //     //setLoading(false);
+      //     return;
+      //   }
+
+      //   const response = await axios.post(url, userData).then((res) => {
+      //     setPlainEmail(res.data.message);
+      //     //setLoading(false);
+      //   });
+
+      //   //console.log("AI reply back: " + response.data.message);
+      // } catch (error) {
+      //   console.log(error);
+      // }
+
+      //setLoading(true);
+      const checkmsg = userData.toLowerCase();
+      console.log("submit msg: " + checkmsg);
+      const keywords = ["email", "letter", "send", "write", "weather"]; // add more keywords
+      if (!keywords.some((keyword) => checkmsg.includes(keyword))) {
+        console.log("submit reppppp dont have kw");
+        setTranscription(
+          "I'm your email bot, ask me anything related to email"
+        );
+        //setLoading(false);
+        return;
+      } else {
+        await axios
+          .post(url, userData)
+          .then(function (response) {
+            setPlainEmail(response.data.message);
+            //setLoading(false);
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+          .finally(function () {
+            console.log("submit end handle plain email");
+          });
+      }
+    },
+    [transcription]
+  );
+
+  // function askAI() {
+  //   return useCallback(
+  //     async (url: string, userData: string) => {
+  //       const checkmsg = userData.toLowerCase();
+  //       console.log("submit msg: " + checkmsg);
+  //       const keywords = ["email", "letter", "send", "write", "weather"]; // add more keywords
+  //       if (!keywords.some((keyword) => checkmsg.includes(keyword))) {
+  //         console.log("submit reppppp dont have kw");
+  //         setTranscription(
+  //           "I'm your email bot, ask me anything related to email"
+  //         );
+  //         //setLoading(false);
+  //         return;
+  //       }
+
+  //       await axios
+  //         .post(url, userData)
+  //         .then(function (response) {
+  //           setPlainEmail(response.data.message);
+  //           //setLoading(false);
+  //         })
+  //         .catch(function (error) {
+  //           console.log(error);
+  //         })
+  //         .finally(function () {
+  //           console.log("submit end handle plain email");
+  //         });
+  //     },
+  //     [transcription]
+  //   );
+  // }
 
   function getDurationFormatted(milliseconds: number) {
     const minutes = milliseconds / 1000 / 60;
@@ -160,46 +293,63 @@ const Main = ({ navigation }: any) => {
     setRecordings([]);
   }
 
+  function LoadingAnimation() {
+    return (
+      <View style={styles.indicatorWrapper}>
+        <ActivityIndicator size="large" />
+        <Text style={styles.title}>Loading your voice...</Text>
+      </View>
+    );
+  }
+
   const onPress = () => {
-    setRecording(recording ? stopRecording : startRecording)
-    setBorderColor(getColour())
+    setRecording(recording ? stopRecording : startRecording);
+    setBorderColor(getColour());
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
-      <Text style={styles.title}>How can I help you?</Text>
-      <Text style={styles.instructions}>
-        Press and hold this button to record your voice. Release the button to
-        send the recording, and you'll hear a response
-      </Text>
- 
-      <TouchableOpacity style={{
-        width: "90%",
-        padding: 30,
-        gap: 10,
-        borderWidth: 3,
-        alignItems: "center",
-        borderRadius: 10,
-        borderColor: borderColor,
-      }}
-        onPress={onPress}>
-        <Image
-        source={require("../../assets/images/robot.gif")}
-        />
-        <Text style={styles.title}> {recording ? "Stop Recording\n\n\n" : "Start Recording\n\n\n"} </Text>
-      </TouchableOpacity>
-      <Text style={styles.title}>Your message: "{transcription}"</Text>
-      { loading ?
-      <ActivityIndicator size="large"/> :
-      <Text style={styles.title}>Your result: "{plainemail}"</Text>
-      }
-      {/* <TouchableOpacity style={{top:90, right:120}}>
-        <MaterialIcons name="record-voice-over" size={30} color="#D6665C" />
-      </TouchableOpacity> */}
-      
+        <Text style={styles.title}>How can I help you?</Text>
+        <Text style={styles.instructions}>
+          Press and hold this button to record your voice. Release the button to
+          send the recording, and you'll hear a response
+        </Text>
+
+        <TouchableOpacity
+          style={{
+            width: "90%",
+            padding: 30,
+            gap: 10,
+            borderWidth: 3,
+            alignItems: "center",
+            borderRadius: 10,
+            borderColor: borderColor,
+          }}
+          onPress={onPress}
+        >
+          <Image source={require("../../assets/images/robot.gif")} />
+          <Text style={styles.title}>
+            {" "}
+            {recording ? "Stop Recording\n\n\n" : "Start Recording\n\n\n"}{" "}
+          </Text>
+        </TouchableOpacity>
+        {loading ? (
+          <LoadingAnimation />
+        ) : (
+          <Text style={styles.title}>Your message: "{transcription}"</Text>
+        )}
+        <Text style={styles.title}>Your result: "{plainemail}"</Text>
       </ScrollView>
-      <TouchableOpacity style={styles.keyboard} onPress={() => navigation.navigate("SendEmail")}>
+      <TouchableOpacity
+        style={styles.keyboard}
+        onPress={() =>
+          navigation.navigate("SendEmail", {
+            subject: "",
+            body: "",
+          })
+        }
+      >
         <FontAwesome5 name="keyboard" size={30} color="#D6665C" />
       </TouchableOpacity>
     </SafeAreaView>
@@ -224,14 +374,14 @@ const styles = StyleSheet.create({
     fontFamily: "Fredoka",
     fontSize: 16,
     paddingTop: 20,
-    paddingBottom: 20
+    paddingBottom: 20,
   },
-  title: { 
+  title: {
     color: theme.colors.textPrimary,
     textAlign: "center",
     fontFamily: "Fredoka",
     fontSize: 20,
-    padding:10
+    padding: 10,
   },
   row: {
     flexDirection: "row",
@@ -247,9 +397,12 @@ const styles = StyleSheet.create({
   keyboard: {
     position: "absolute",
     bottom: 12,
-    // top:60, 
+    // top:60,
     // left:120,
-
-
-  }
+  },
+  indicatorWrapper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const formData = require("form-data");
 const Mailgun = require("mailgun.js");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs");
@@ -14,24 +14,24 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 // Imports the ChatAI library
 const { Configuration, OpenAI } = require("openai");
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(cors());
 
-console.log("Config started")
+console.log("Config started");
 
 // Creates the config for OpenAI
 // const newConfig = new Configuration({
 //     apiKey: process.env.OPENAI_API_KEY,
 // });
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 // Creates a client by Google Cloud
@@ -39,142 +39,143 @@ const client = new speech.SpeechClient();
 
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({
-	username: 'api',
-	key: process.env.MAILGUN_API_KEY,
+  username: "api",
+  key: process.env.MAILGUN_API_KEY,
 });
 
-console.log("Configuration is ready!")
+console.log("Configuration is ready!");
 
 const getResponseAI = async (text) => {
-	const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-			{
-                role: "system",
-                content: "You are a helpful assistant."
-            },
-			{ role: "user", 
-			content: text, 
-			}],
-    });
-	return response.choices[0].message.content;
-}
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      { role: "user", content: text },
+    ],
+  });
+  return response.choices[0].message.content;
+};
 
 app.get("/api/", (req, res) => {
-	res.json("Hello World");
-  });
+  res.json("Hello World");
+});
 
 app.post("/api/transcribe", async (req, res) => {
-	try {
-	  const pathDesktop = "/Users/saran/Desktop/test1mp3.mp3";
-	  const receivedData = req.body;
-  
-	  const splitString = JSON.stringify(receivedData).split(":");
-	  const slicename = splitString[1].slice(2, -1).trim();
-	  const nameFile = slicename.split(".")[0].split("/").pop();
-  
-	  //console.log(`Link URI: ${JSON.stringify(receivedData)}`);
-	  //console.log("splice filename string: " + slicename);
-	  //console.log("filename string: " + nameFile);
-  
-	  const audiosDir = "./audios";
-	  const outputPath = path.join(
-		audiosDir,
-		`${path.basename(`${nameFile}`, path.extname(`${nameFile}`))}.mp3`
-	  );
-  
-	  if (!fs.existsSync(slicename)) {
-		console.log("BAD");
-	  } else {
-		console.log("GOOD");
-	  }
-  
-	  ffmpeg(slicename)
-		.outputOptions("-vn", "-ab", "128k", "-ar", "44100")
-		.toFormat("mp3")
-		.save(outputPath)
-		.on("error", (err) => console.error(`Error converting file: ${err}`))
-		.on("end", () => console.log(`Converted ${nameFile}`));
-  
-	  // config audio
-	  const filename = pathDesktop; //test data
-	//   const filename = slicename;
-	  const encoding = "MP3";
-	  const sampleRateHertz = 16000;
-	  const languageCode = "en-US";
-  
-	  const config = {
-		encoding: encoding,
-		sampleRateHertz: sampleRateHertz,
-		languageCode: languageCode,
-	  };
-  
-	  const audio = {
-		content: fs.readFileSync(filename).toString("base64"),
-	  };
-  
-	  const request = {
-		config: config,
-		audio: audio,
-	  };
-  
-	  // Detects speech in the audio file
-	  const [response] = await client.recognize(request);
-	  const transcription = response.results
-		.map((result) => result.alternatives[0].transcript)
-		.join("\n");
-	  console.log("Transcription: ", transcription);
-  
-	  res.status(200).send({"transcription" : transcription});
-	} catch (error) {
-	  console.error("Error:", error);
-	  res.status(500).json({ error: "Internal Server Error" });
-	}
+  try {
+    //const pathDesktop = "/Users/saran/Desktop/test1mp3.mp3";
+    const pathDesktop = "/Users/quanle/Desktop/test1mp4.mp3";
+    const receivedData = req.body;
+
+    const splitString = JSON.stringify(receivedData).split(":");
+    const slicename = splitString[1].slice(2, -1).trim();
+    const nameFile = slicename.split(".")[0].split("/").pop();
+
+    //console.log(`Link URI: ${JSON.stringify(receivedData)}`);
+    //console.log("splice filename string: " + slicename);
+    //console.log("filename string: " + nameFile);
+
+    // const audiosDir = "./audios";
+    // const outputPath = path.join(
+    //   audiosDir,
+    //   `${path.basename(`${nameFile}`, path.extname(`${nameFile}`))}.mp3`
+    // );
+
+    // if (!fs.existsSync(slicename)) {
+    //   console.log("BAD");
+    // } else {
+    //   console.log("GOOD");
+    // }
+
+    // ffmpeg(slicename)
+    //   .outputOptions("-vn", "-ab", "128k", "-ar", "44100")
+    //   .toFormat("mp3")
+    //   .save(outputPath)
+    //   .on("error", (err) => console.error(`Error converting file: ${err}`))
+    //   .on("end", () => console.log(`Converted ${nameFile}`));
+
+    // config audio
+    const filename = pathDesktop; //test data
+    //   const filename = slicename;
+    const encoding = "MP3";
+    const sampleRateHertz = 16000;
+    const languageCode = "en-US";
+
+    const config = {
+      encoding: encoding,
+      sampleRateHertz: sampleRateHertz,
+      languageCode: languageCode,
+    };
+
+    const audio = {
+      content: fs.readFileSync(filename).toString("base64"),
+    };
+
+    const request = {
+      config: config,
+      audio: audio,
+    };
+
+    // Detects speech in the audio file
+    const [response] = await client.recognize(request);
+    const transcription = response.results
+      .map((result) => result.alternatives[0].transcript)
+      .join("\n");
+    console.log("Transcription: ", transcription);
+
+    res.status(200).send({ transcription: transcription });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.post("/api/ask", async (req, res) => {
-	const receivedData = req.body;
-	const splitString = JSON.stringify(receivedData).split(":");
-	const userPrompt = splitString[0].trim();
-	//console.log("messge from ask: " + splitString)
+  const receivedData = req.body;
+  const splitString = JSON.stringify(receivedData).split(":");
+  const userPrompt = splitString[0].trim();
+  //console.log("messge from ask: " + splitString)
 
-	try {
-		if (userPrompt == null) {
-		  throw new Error("Uh oh, no prompt was provided");
-		}
-		const response = await getResponseAI(userPrompt);
-		console.log("AI rep: " + response);
-		res.status(200).send({
-		  success: true,
-		  message: response,
-		});
-	} catch (error) {
-		console.log("ChatGPT error: " + error.message);
-	}
-})
+  try {
+    if (userPrompt == null) {
+      throw new Error("Uh oh, no prompt was provided");
+    }
+    const response = await getResponseAI(userPrompt);
+    console.log("AI rep: " + response);
+    res.status(200).send({
+      success: true,
+      message: response,
+    });
+  } catch (error) {
+    console.log("ChatGPT error: " + error.message);
+  }
+});
 
-app.post('/api/email', (request, response)=>{
-    const { sender, recipients, subject, content } = request.body;
+app.post("/api/email", (request, response) => {
+  const { sender, recipients, subject, content } = request.body;
 
-    mg.messages.create(process.env.MAILGUN_DOMAIN, {
-		from: `"GTalkie" ${sender}`,
-        to: `${recipients}`,
-        subject: `${subject}`,
-        html: `${content}`,
-	})
-	.then(msg => {
-		console.log(msg); // logs response data
-        response.write("<h1>You have successfully subscribed!</h1>");
-        response.end();
-	})
-	.catch(err => {
-		console.log(err); // logs any error
-		response.status(500).send('Error sending email.');
-	});
+  mg.messages
+    .create(process.env.MAILGUN_DOMAIN, {
+      from: `"GTalkie" ${sender}`,
+      to: `${recipients}`,
+      subject: `${subject}`,
+      html: `${content}`,
+    })
+    .then((msg) => {
+      console.log(msg); // logs response data
+      response.write("<h1>You have successfully subscribed!</h1>");
+      response.end();
+    })
+    .catch((err) => {
+      console.log(err); // logs any error
+      response.status(500).send("Error sending email.");
+    });
 });
 
 const port = 4000;
 
-app.listen(port, function(request, response){
-    console.log(`server is running on port ${port}`)
+app.listen(port, function (request, response) {
+  console.log(`server is running on port ${port}`);
 });
